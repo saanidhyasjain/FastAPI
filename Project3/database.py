53d94c42b6d8aa6d45250a6c9966f2047080e5c0
saanidhyasjain <saanidhya0902@gmail.com>
2026-01-13 21:33:43 +0530

    prodution database change

--- Project3/database.py
+++ Project3/database.py
@@ -2,14 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
 
 
-#POSTGRESQL_DATABASE_URL = 'postgresql://postgres:test1234:@localhost/TodoApplicationDatabase'
+POSTGRESQL_DATABASE_URL = 'postgresql://deploy_database_yn0f_user:Tq8rgmt4z7DxVVisqGzI6Euuglz8o50B@dpg-d5j6lqogjchc73cvb3dg-a/deploy_database_yn0f'
 # MYSQL_DATABASE_URL = 'mysql+pymysql://root:test1234@127.0.0.1:3306/todoapplicationdatabase'
 #  connect_args={'check_same_thread':False} this will allow sqlite to communicate only with one thread
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+engine = create_engine(POSTGRESQL_DATABASE_URL, connect_args={'check_same_thread':False})
+
 #engine = create_engine(MYSQL_DATABASE_URL)
 
 
